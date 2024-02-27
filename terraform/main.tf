e4a20698e30388ba0190e45497e5acaca0a7cb3e
Fabian Brakowski <fbrakowski@tecracer.de>
2024-02-27 08:52:13 +0100

    bugfix + python version update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "../lambda_function.py"
+  source_file = "${path.module}/../lambda_function.py"
   output_path = "temp_lambda_function.zip"
 }
 
@@ -51,7 +51,7 @@ resource "aws_iam_role_policy_attachment" "ec2_stop_instances_policy_attachment"
 resource "aws_lambda_function" "lambda" {
   function_name = "ec2_stop_instances_unless_tagged"
   handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
+  runtime       = "python3.12"
   role          = aws_iam_role.iam_for_lambda.arn
   filename      = data.archive_file.lambda_zip.output_path
   timeout       = 60
